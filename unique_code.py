2169cbb0883b6080e5a871bb603c8e23f88eda4d
jpe793 <jpe793@inlumine.ual.es>
2026-05-15 14:05:07 +0200

    New branch and modified file

--- unique_code.py
+++ unique_code.py
@@ -1,5 +1,5 @@
 def unique_code(text):
-    code = ""
-    for letter in text:
-        code += str(ord(letter))
-    return code
+code = ""
+for letter in text:
+    code += str(ord(letter) + 1) # new feature: shifts each letter
+return code
