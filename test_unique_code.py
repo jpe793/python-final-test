1f42ebdd20fd876888a57041bd78abca04e706dd
jpe793 <jpe793@inlumine.ual.es>
2026-05-15 14:27:13 +0200

    Corrected files for test

--- test_unique_code.py
+++ test_unique_code.py
@@ -1,7 +1,9 @@
 import unittest
-from unique_code import unique_code 
-class TestUniqueCode(unittest.TestCase): 
-    def test_alex(self): 
-        self.assertEqual(unique_code("Alex"), "66109102121")
+from unique_code import unique_code
+
+class TestUniqueCode(unittest.TestCase):
+    def test_alex(self):
+        self.assertEqual(unique_code("Alex"), "65108101120")
+
 if __name__ == "__main__":
     unittest.main()
